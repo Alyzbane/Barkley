0270c1ee4bfb94161c2a98dfe95404b300eb37f4
alyzbane <alyzbane@gmail.com>
2025-04-22 23:06:21 +0800

    Add Git LFS metadata for model files in florence-2-base directory and change the redis ttl

---
+++ models/florence-2-base/README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e49de5527bd39688745ce20388bf3f31fdb891abc0e6950a3f64630114769d17
+size 14805
---
+++ models/florence-2-base/modeling_florence2.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8727dfdb55a2b05f7a0234c2a26727ec7fbcefe7f81c89c2810ad14109143a9c
+size 127294
